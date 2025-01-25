bcefebd6e22e636099c1d39f59907783338b1c19
Marti <Marti@HUAWEI>
2025-01-25 16:55:24 +0300

    Добавьте файлы проекта.

---
+++ 1pk1_Martynenko/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1pk1_Martynenko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
