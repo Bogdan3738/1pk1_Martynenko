6564118b95cf99c3b8dc059f459a8a61ca507bf7
Martynenko Bogdan <bmart430@gmail.com>
2025-01-27 04:32:47 +0300

    Задание 8 занятия 5 выполнено

---
+++ Task_05_08/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_05_08
+{
+    internal class Program
+    {
+        /*Даны два массива размером 10*10, заполненные целыми числами в диапазоне от 1 до 9 вкл.
+         * Создать новый массив, который будет
+         * произведением двух предыдущих (перемножить элементы первых двух массивов, стоящие на одинаковых позициях и записать их в
+         * результирующий массив)
+         * Вывести результирующий массив 
+         */
+        static void Main(string[] args)
+        {
+            static void PrintMatrix(int[,] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static void PrintMatrixWithHighlights(int[,] matrix, int[] highlights)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (Array.Exists(highlights, element => element == matrix[i, j]))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        Console.Write(matrix[i, j] + "\t");
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
