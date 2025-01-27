52eb1a8eeedac1d7a83e3b7934609c56163637e4
Martynenko Bogdan <bmart430@gmail.com>
2025-01-27 04:10:03 +0300

    Задание 9 занятия 4 выполнено

---
+++ Task_04_09/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task_04_09
+{
+    internal class Program
+
+    { /* В массиве найти элементы, которые в нем встречаются только один раз, 
+       * и вывести их на экран. То есть найти и
+       * вывести уникальные элементы массива. (LINQ использовать нельзя)
+
+       */
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 21);
+            }
+
+            Console.WriteLine("Массив: " + string.Join(", ", array));
+
+            int[] uniqueElements = new int[array.Length];
+            int uniqueCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isUnique = true;
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (i != j && array[i] == array[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+
+                if (isUnique)
+                {
+                    uniqueElements[uniqueCount] = array[i];
+                    uniqueCount++;
+                }
+            }
+
+            int[] result = new int[uniqueCount];
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                result[i] = uniqueElements[i];
+            }
+
+            Console.WriteLine("Уникальные элементы: " + string.Join(", ", result));
+        }
+    }
+}
