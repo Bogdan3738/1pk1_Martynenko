531a34ab1ac5e39284fef6b316c9a74347afe83d
Martynenko Bogdan <bmart430@gmail.com>
2025-01-27 04:07:46 +0300

    Заадание 8 занятия 4 выполнено

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    { /*Дан массив, содержащий последовательность 50 случайных чисел.
+             * Найти количество пар элементов, значения которых равны.
+             */
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 21);
+            }
+
+            int pairCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                        pairCount++;
+                }
+            }
+
+            Console.WriteLine("Массив: " + string.Join(", ", array));
+            Console.WriteLine("Количество пар: " + pairCount);
+        }
+    }
+}
