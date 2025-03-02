5a1b20e122a70220778bc79528e72fd54db6d30d
Martynenko Bogdan <bmart430@gmail.com>
2025-03-02 20:47:56 +0300

    Задание 8 лекции 10 выполнено

---
+++ Task_10_08/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_10_08
+{
+    /*Создайте метод, который на вход принимает одномерный массив и число для поиска,
+     * верните индекс искомого элемента в массиве. Если элемента нет – верните индекс = -1
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] array = {-3,4,5,0,8,3,90,10,200};
+            Console.WriteLine(GetIndex(array,200));
+        }
+        static int GetIndex(int[] ind, int n)
+        { 
+        for(int i = 0; i < ind.GetLength(0); i++)
+            {
+                if(ind[i] == n) return i;
+                
+            }
+        return -1;
+            
+        }
+    }
+}
