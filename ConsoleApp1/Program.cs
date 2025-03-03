bdf5ffa8b01386b24f395459d498173547004466
Martynenko Bogdan <bmart430@gmail.com>
2025-03-03 22:17:17 +0300

    Задание 1 лекции 11 выполнено

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine($"До изменения: a = {a}, b = {b}");
+
+        Swap(ref a, ref b); // передаем переменные по ссылке
+
+        Console.WriteLine($"После изменения: a = {a}, b = {b}");
+    }
+
+    static void Swap(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+}
---
+++ Task_11_01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_11_01
+{
+    /*
+     Передача по значению: Напишите метод, который принимает два целых числа
+    и меняет их местами. Проверьте, изменились ли значения переменных вне метода
+     */
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine("До изменения: a = {a}, b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine("После изменения: a = {a}, b = {b}");
+        }
+
+        static void Swap(ref int i, ref int j)
+        {
+            int temp = i;
+            i = j;
+            j = temp;
+        }
+    }
+
+
+}
