65347258275ff53cf7f8d3b43bd32412f2424f82
Martynenko Bogdan <bmart430@gmail.com>
2025-03-05 07:50:03 +0300

    Задание 3 лекции 14 выполнено

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_14_03
+{
+    /*
+     Реализуйте статический метод Factorial, который принимает целое число
+    и возвращает его факториал. Сделайте так, чтобы метод работал только для неотрицательных чисел.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Factorial(5));
+
+        }
+        static int Factorial(int x)
+        {
+            int y = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                y *= i;
+            }
+
+            return y;
+
+
+        }   
+    }
+}
