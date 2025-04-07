bcb2f72624b5419573bee9a50e97c6bc943f11ad
Martynenko Bogdan <bmart430@gmail.com>
2025-04-07 19:16:06 +0300

    Задание 05 лекции 20 выполнено

---
+++ Task_20_05/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_20_05
+{
+    /*Напишите программу, имитирующую систему авторизации с использованием перечисления AccessLevel:
+• Guest (только чтение)
+• User (чтение + комментарии)
+• Moderator (удаление контента)
+• Admin (полный доступ)
+Создайте метод, который проверяет, может ли пользователь выполнить действие (например, удалитьпост).На основе уровня доступа выводите сообщение (например,
+"Ошибка: Недостаточно прав!").
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User guest = new User("Guest User", AccessLevel.Guest);
+            User user = new User("Regular User", AccessLevel.User);
+            User moderator = new User("Moderator User", AccessLevel.Moderator);
+            User admin = new User("Admin User", AccessLevel.Admin);
+
+            // Проверяем действия разных пользователей
+            CheckUserAction(guest, "delete_post");
+            CheckUserAction(user, "delete_post");
+            CheckUserAction(moderator, "delete_post");
+            CheckUserAction(admin, "delete_post");
+        }
+
+        static void CheckUserAction(User user, string action)
+        {
+            if (user.CanPerformAction(action))
+            {
+                Console.WriteLine($"{user.Name} может выполнить действие: {action}");
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: {user.Name} не имеет прав на выполнение действия: {action}");
+            }
+        }
+    }
+}
+
---
+++ Task_20_05/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_05
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public AccessLevel Level { get; set; }
+
+        public User(string name, AccessLevel level)
+        {
+            Name = name;
+            Level = level;
+        }
+
+        // Метод для проверки прав на выполнение действия
+        public bool CanPerformAction(string action)
+        {
+            switch (action.ToLower())
+            {
+                case "delete_post":
+                    return Level == AccessLevel.Moderator || Level == AccessLevel.Admin;
+                case "edit_post":
+                    return Level == AccessLevel.Admin;
+                case "comment":
+                    return Level == AccessLevel.User || Level == AccessLevel.Moderator || Level == AccessLevel.Admin;
+                case "view":
+                    return Level != AccessLevel.Guest; 
+                default:
+                    return false;
+            }
+        }
+    }
+}
