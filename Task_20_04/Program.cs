0530a3aae3cdd7c94c17a45c4ab4689d004e8ab7
Martynenko Bogdan <bmart430@gmail.com>
2025-04-07 19:06:51 +0300

    Задание 4 лекции 20 выполнено

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_20_04
+{
+    /*Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+• Car
+• Bike
+• Bus
+• Truck
+• Motorcycle
+Храните список транспортных средств (можно просто List<VehicleType>).
+    Добавьте метод для подсчёта транспорта определённого типа
+    (например, сколько грузовиков). Реализуйте поиск по типу и вывод информации.
+     
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleRegistry registry = new VehicleRegistry();
+
+            // Добавляем транспортные средства
+            registry.AddVehicle(VehicleType.Car);
+            registry.AddVehicle(VehicleType.Bike);
+            registry.AddVehicle(VehicleType.Bus);
+            registry.AddVehicle(VehicleType.Truck);
+            registry.AddVehicle(VehicleType.Motorcycle);
+            registry.AddVehicle(VehicleType.Truck); // Добавляем еще один грузовик
+
+            // Выводим список всех транспортных средств
+            registry.DisplayVehicles();
+
+            // Подсчёт количества грузовиков
+            int truckCount = registry.CountVehicles(VehicleType.Truck);
+            Console.WriteLine($"Количество грузовиков: {truckCount}");
+
+            // Подсчёт количества мотоциклов
+            int motorcycleCount = registry.CountVehicles(VehicleType.Motorcycle);
+            Console.WriteLine($"Количество мотоциклов: {motorcycleCount}");
+        }
+    }
+}
---
+++ Task_20_04/VehicleRegistry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_04
+{
+    public class VehicleRegistry
+    {
+        private List<VehicleType> _vehicles;
+
+        public VehicleRegistry()
+        {
+            _vehicles = new List<VehicleType>();
+        }
+        public void AddVehicle(VehicleType vehicle)
+        {
+            _vehicles.Add(vehicle);
+        }
+
+        // Метод для подсчета транспортных средств определённого типа
+        public int CountVehicles(VehicleType vehicleType)
+        {
+            return _vehicles.Count(v => v == vehicleType);
+        }
+
+        // Метод для вывода информации о всех транспортных средствах
+        public void DisplayVehicles()
+        {
+            Console.WriteLine("Список транспортных средств:");
+            foreach (var vehicle in _vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+    }
+}
