4e408b0c72bfb60bfd0e7ddb646dd00320a14bd4
Martynenko Bogdan <bmart430@gmail.com>
2025-04-07 19:27:19 +0300

    Задание 01 лекции 21 выполнено

---
+++ Task_21_01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_21_01
+{ /*Дан список чисел.
+   * Создать новый список, содержащий только чётные числа из исходного списка, умноженныена 10.
+   */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Исходный список чисел
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Новый список, содержащий чётные числа, умноженные на 10
+            List<int> evenNumbersMultipliedBy10 = numbers
+                .Where(n => n % 2 == 0) // Фильтр на чётные числа
+                .Select(n => n * 10)    // Умножаем каждое чётное число на 10
+                .ToList();              // Преобразуем в список
+
+            // Вывод результата
+            Console.WriteLine("Чётные числа, умноженные на 10:");
+            foreach (var number in evenNumbersMultipliedBy10)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
